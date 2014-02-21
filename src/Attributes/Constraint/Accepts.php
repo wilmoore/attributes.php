21fc5255a87485f5155f835ccda83c42fd372540
Travis Elkins <travis.elkins+sourcetree@gmail.com>
2014-02-21 15:17:40 +0100

    Cleaned up debug code.

--- src/Attributes/Constraint/Accepts.php
+++ src/Attributes/Constraint/Accepts.php
@@ -15,9 +15,6 @@ class Accepts extends AbstractConstraint implements ConstraintInterface
         } elseif (is_string($accepts)) {
             // numeric range
             if (preg_match('/^(?P<start>-*\d+)[.][.](?P<limit>-*\d+)$/', $accepts, $matches)) {
-//$range = range($matches['start'], $matches['limit'], 1);
-//echo "Range: " . print_r($range, true) . PHP_EOL;
-//exit;
                 return in_array($value, range($matches['start'], $matches['limit'], 1), true);
             } else {
                 return preg_match("/{$accepts}/u", $value);
