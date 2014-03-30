9853fd921c8cb9ff05a74cebe34766fb2601417e
Travis Elkins <travis.elkins+sourcetree@gmail.com>
2014-03-30 18:41:59 +0200

    Added minimum/maximum constraints.

---
+++ test/Constraint/MinimumTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Test\Constraint;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class MinimumTest extends TestCase
+{
+
+    /**
+     * Object Instance Wrapper
+     *
+     * adds an object instance to each incoming hash
+     *
+     * @param   array   attribute data provider configuration
+     *
+     * @return  array
+     */
+    function instanceWrapper($data)
+    {
+        return array_map(function($parameters) {
+            $constraint = $parameters[0];
+            $instance = new \Metaphp\Attributes\Constraint\Minimum($constraint);
+            $parameters[] = $instance;
+
+            return $parameters;
+        }, $data);
+    }
+
+    /**
+     * attribute below minimum
+     *
+     * fields:
+     *  - [numeric] maximum constraint
+     *  - [mixed] value to test
+     *
+     * @return array
+     */
+    public function providerInvalidValues()
+    {
+        $data[] = [100, '99.9'];
+        $data[] = [100, 99.9];
+        $data[] = [-100, '-200'];
+        $data[] = [-100, -200];
+        $data[] = [0, '-0.1'];
+        $data[] = [0, -0.1];
+//        $data[] = ['^element-property-', 'props'];
+
+        return $this->instanceWrapper($data);
+    }
+
+    /**
+     * @test
+     * @dataProvider providerInvalidValues
+     */
+    function invalidValues($constraint, $value, $instance)
+    {
+        $this->assertFalse($instance->isValid($value));
+    }
+
+    /**
+     * attribute greater than minimum
+     *
+     * fields:
+     *  - [numeric] accepts constraint
+     *  - [mixed] value to test
+     *
+     * @return array
+     */
+    public function providerValidValues()
+    {
+        $data[] = [100, '100'];
+        $data[] = [100, '100.0'];
+        $data[] = [-100, '-100'];
+        $data[] = [-100, '-100.0'];
+        $data[] = [100, 100];
+        $data[] = [100, 100.0];
+        $data[] = [-100, -100];
+        $data[] = [-100, -100.0];
+        $data[] = [100, '101'];
+        $data[] = [100, '100.1'];
+        $data[] = [-100, '-99.9'];
+        $data[] = [100, 101];
+        $data[] = [100, 100.1];
+        $data[] = [-100, -99.9];
+        $data[] = [0, '0.1'];
+        $data[] = [0, 0.1];
+
+        return $this->instanceWrapper($data);
+    }
+
+    /**
+     * @test
+     * @dataProvider providerValidValues
+     */
+    function validValues($constraint, $value, $instance)
+    {
+        $this->assertTrue($instance->isValid($value));
+    }
+
+}
